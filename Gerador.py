0c8ea34d63873d0f6fd7504b2aef64cdc546b221
Lucas Borges <86315861+Lusqinha@users.noreply.github.com>
2022-07-29 21:00:25 -0300

    Adição de nova funcionafuncionalidade 🐱‍🏍
    Adicionada a funcionalidade de excluir automaticamente as etiquetas não emitidas pelo meu setor, assim como a melhor implementação do código.

--- Gerador.py
+++ Gerador.py
@@ -4,23 +4,33 @@ from docx.shared import Pt
 
 reader = PdfReader("etiquetas.pdf")
 text = ""
-pag = 0
+paginas = 0
 
 document = Document()
+style = document.styles['Normal']
+font = style.font
+font.name = 'Cambria (Corpo)'
+font.size = Pt(10)
 
 for page in reader.pages:
   text+= page.extract_text() + "\n"
-  pag += 1
+  paginas += 1
 
-textoApagar = "|-APAGAR-|_|-APAGAR-|_|-APAGAR-|_|-APAGAR-|"
+
+textoApagar = "|-APAGAR-|"
 correcoes = [
               "Locação Maq",
               "FORMALIZAÇÃO EMPRESA",
               "Alvará local.Funcion.CNPJ",
-              "005 -SECFAZ/Fiscalização",
-              "004 -SECFAZ/Arrecadação",
+              "SECFAZ/Fiscalização",
+              "SECFAZ/Arrecadação",
+              "018 -SECFAZ / Fiscalização",
+              "010-Isenção da Taxa de Entulho",
               "015-Patrulha Agricola",
-              "028-FORMALIZAÇÃO EMPRESAS-REDESIM"
+              "028-FORMALIZAÇÃO EMPRESAS-REDESIM",
+              "029-FORMALIZAÇÃO EMPRESA - MEI",
+              "030-ALTERAÇÃO - MEI",
+              "031-ALTERAÇÃO - REDESIM"
             ]
 
 for string in correcoes:
@@ -28,17 +38,49 @@ for string in correcoes:
 
 text = text.replace("Documento..: ", "")
 
-print(text)
+# ------------Código de teste --------------- #
 
-print(f"{text.count(textoApagar)} etiquetas foram identificadas como 'para apagar'")
+class Etiqueta:
+  def __init__(self):
+    self.listaEtiquetas = []
+    self.assunto = ''+'\n'
+    self.subassunto = ''+'\n'
+    self.requerente = ''+'\n'
+  
+  def __str__(self):
+    return {"assunto....":self.assunto.removeprefix("Assunto....: "), "subassunto.":self.subassunto.removeprefix("Subassunto.: "), "requerente.":self.requerente.removeprefix("Requerente.: ")}
+  
+  def geradorEtiqueta(self, t):
+    for line in t.split('\n'):
+      if line.startswith("Assu"):
+        self.assunto = line
+        self.listaEtiquetas.append(self.assunto)
+      elif line.startswith("Suba"):
+        self.subassunto = line
+        self.listaEtiquetas.append(self.subassunto)
+      elif line.startswith("Requ"):
+        self.requerente = line
+        self.listaEtiquetas.append(self.requerente)
+    return self.listaEtiquetas
+    
+  
+etiqueta = Etiqueta()
+etiqueta.geradorEtiqueta(text)
 
-style = document.styles['Normal']
-font = style.font
-font.name = 'Cambria (Corpo)'
-font.size = Pt(10)
+indexUm = 0
+indexDois = 3
 
-p = document.add_paragraph(text)
+while paginas >= 0:
+  tag = etiqueta.listaEtiquetas[indexUm:indexDois]
+  paginas-=1
+  if textoApagar in str(tag):
+    str(tag).replace(str(tag), "")
+  else:
+    tag = '\n'.join([i for i in tag[0:]])
+    p = document.add_paragraph(tag)
+    
+  indexUm += 3
+  indexDois += 3
 
 p.style = document.styles['Normal']
-
 document.save("etiqueta.docx")
