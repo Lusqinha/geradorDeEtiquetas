28c7b9be720ab90abb8677c4fac016baec97af68
Lucas Borges <86315861+Lusqinha@users.noreply.github.com>
2022-09-15 11:23:58 -0300

    Melhora na funcionalidade🎢
    Capturando qualquer arquivo PDF e mais informações para filtragem

--- Gerador.py
+++ Gerador.py
@@ -1,21 +1,14 @@
 from PyPDF2 import PdfReader
 from docx import Document
 from docx.shared import Pt
+from glob import glob
 
-reader = PdfReader("etiquetas.pdf")
+
+pdFile = glob('*.pdf')[0]
+
+reader = PdfReader(pdFile)
 text = ""
 paginas = 0
-
-document = Document()
-style = document.styles['Normal']
-font = style.font
-font.name = 'Cambria (Corpo)'
-font.size = Pt(10)
-
-for page in reader.pages:
-  text+= page.extract_text() + "\n"
-  paginas += 1
-
 
 textoApagar = "|-APAGAR-|"
 correcoes = [
@@ -26,32 +19,50 @@ correcoes = [
               "SECFAZ/Arrecadação",
               "018 -SECFAZ / Fiscalização",
               "010-Isenção da Taxa de Entulho",
+              "Taxa de Entulho",
+              "AUTONOMO",
+              "Construção de Tumulo",
               "015-Patrulha Agricola",
+              "Patrulha",
               "028-FORMALIZAÇÃO EMPRESAS-REDESIM",
               "029-FORMALIZAÇÃO EMPRESA - MEI",
               "030-ALTERAÇÃO - MEI",
-              "031-ALTERAÇÃO - REDESIM"
+              "030 -ALTERAÇÃO",
+              "031-ALTERAÇÃO - REDESIM",
+              "031 -ALTERAÇÃO - REDESIM",
+              "Baixa de lotação",
+              "Procuradoria Geral",
+              "PGM"
             ]
+
+document = Document()
+style = document.styles['Normal']
+font = style.font
+font.name = 'Cambria (Corpo)'
+font.size = Pt(11)
+
+for page in reader.pages:
+  text+= page.extract_text() + "\n"
+  paginas += 1
 
 for string in correcoes:
   text = text.replace(string, textoApagar)
 
 text = text.replace("Documento..: ", "")
 
-# ------------Código de teste --------------- #
-
 class Etiqueta:
   def __init__(self):
     self.listaEtiquetas = []
+    self.protocolo = '' + '\n'
     self.assunto = ''+'\n'
     self.subassunto = ''+'\n'
     self.requerente = ''+'\n'
   
-  def __str__(self):
-    return {"assunto....":self.assunto.removeprefix("Assunto....: "), "subassunto.":self.subassunto.removeprefix("Subassunto.: "), "requerente.":self.requerente.removeprefix("Requerente.: ")}
-  
   def geradorEtiqueta(self, t):
     for line in t.split('\n'):
+      if line.startswith("2022"):
+        self.protocolo = line
+        self.listaEtiquetas.append(self.protocolo)
       if line.startswith("Assu"):
         self.assunto = line
         self.listaEtiquetas.append(self.assunto)
@@ -68,7 +79,8 @@ etiqueta = Etiqueta()
 etiqueta.geradorEtiqueta(text)
 
 indexUm = 0
-indexDois = 3
+indexDois = 4
+
 
 while paginas >= 0:
   tag = etiqueta.listaEtiquetas[indexUm:indexDois]
@@ -79,8 +91,8 @@ while paginas >= 0:
     tag = '\n'.join([i for i in tag[0:]])
     p = document.add_paragraph(tag)
     
-  indexUm += 3
-  indexDois += 3
+  indexUm += 4
+  indexDois += 4
 
 p.style = document.styles['Normal']
 document.save("etiqueta.docx")
